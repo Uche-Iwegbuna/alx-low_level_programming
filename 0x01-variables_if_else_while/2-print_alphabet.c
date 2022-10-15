8cc5c64c88986940f9e4fc0e952e2f4af1123795
Uche-Iwegbuna <uchechialvinaiwegbuna@gmail.com>
2022-10-15 15:29:51 -0700

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	cchar ls;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
