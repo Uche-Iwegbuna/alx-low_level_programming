820322686b910439fff84ec75d3dcc6416d9f578
Uche-Iwegbuna <uchechialvinaiwegbuna@gmail.com>
2022-10-20 03:15:12 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,9 +9,10 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 == 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
