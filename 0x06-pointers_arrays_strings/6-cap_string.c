0ba8c41237550874ddaab1dcbf6469a0cfd56569
Uche-Iwegbuna <uchechialvinaiwegbuna@gmail.com>
2022-10-30 07:14:22 -0700

    edit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,6 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-			
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
@@ -30,6 +29,8 @@ char *cap_string(char *str)
 				str[i - 1] == '}' ||
 				i == 0)
 			str[i] -= 32;
+
+		i++;
 	}
 	
 	return (str);
