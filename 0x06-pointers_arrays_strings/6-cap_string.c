bac96fa0424e17a35cb67968daff11e45e290ef5
Uche Iwegbuna <88087019+Uche-Iwegbuna@users.noreply.github.com>
2022-10-30 14:33:56 +0100

    Update 6-cap_string.c
    save

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,8 @@
 
 /**
  * cap_string - capitalizes words
- * @str: string
+ * @str: string to be capitalized
+ *
  * Return: pointer to string
  */
 char *cap_string(char *str)
@@ -13,7 +14,6 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-			
 			
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
@@ -30,6 +30,8 @@ char *cap_string(char *str)
 				str[i - 1] == '}' ||
 				i == 0)
 			str[i] -= 32;
+			
+			i++;
 	}
 
 	return (str);
