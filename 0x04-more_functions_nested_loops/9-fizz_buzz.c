8c76fe549fab8f12b16deba875616e5dfb1290bf
Uche-Iwegbuna <uchechialvinaiwegbuna@gmail.com>
2022-10-30 08:20:50 -0700

    updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,19 @@ int main(void)
 	
 	for (n = 1; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (n % 3 == 0 && n % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (n % 5 == 0 && n % 3 != 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i == 1)
+		else if (n == 1)
 		{
 			printf("%d", n);
 		}
