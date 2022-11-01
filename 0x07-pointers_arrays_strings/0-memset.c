db93c447562c5ac8597be4978587cacbeecc9e79
Uche-Iwegbuna <uchechialvinaiwegbuna@gmail.com>
2022-11-01 08:50:07 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - fill memory block with a value
+ * *@s: adress to be filled
+ * *@b: desired value
+ * *@n: number of byte to be changed
+ * Return: change array of n bytes with new values
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	
+	for (; n > 0; i++)
+	{
+			s[i] = b;
+			n--;
+	}
+	return (s);
+}
